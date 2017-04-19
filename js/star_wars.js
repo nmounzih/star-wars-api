bddc4b6ef7e1c31b5f8ca1201fd105b38cbf0f33
Nadia Mounzih <nmounzih@yahoo.com>
2017-04-19 13:54:20 -0400

    Fixed bugs of divs having same id numbers

--- js/star_wars.js
+++ js/star_wars.js
@@ -18,18 +18,25 @@ function getPersonDetail(p){
   }
   $.ajax(settings).done(function(result){
     let name = result.name;
+    let height = result.height;
+    let hair_color = result.hair_color;
+    let skin_color = result.skin_color;
+    let eye_color = result.eye_color;
+    let gender = result.gender;
     let birth_year = result.birth_year;
     let films = result.films;
-    let species = result.species;
-    let vehicles = result.vehicles;
-    let starships = result.starships;
+    // let species = result.species;
+    // let starships = result.starships;
     $('#'+id_num).empty();
     $('#'+id_num).append(name + '<br>');
+    $('#'+id_num).append(height + '<br>');
+    $('#'+id_num).append(hair_color + '<br>');
+    $('#'+id_num).append(eye_color + '<br>');
+    $('#'+id_num).append(gender + '<br>');
     $('#'+id_num).append(birth_year + '<br>');
-    $('#'+id_num).append(films + '<br>');
-    $('#'+id_num).append(species + '<br>');
-    $('#'+id_num).append(vehicles + '<br>');
-    $('#'+id_num).append(starships + '<br>');
+    // $('#'+id_num).append(films + '<br>');
+    // $('#'+id_num).append(species + '<br>');
+    // $('#'+id_num).append(starships + '<br>');
   })
 }
 
@@ -41,7 +48,7 @@ function getFilms(f){
   }
   $.ajax(settings).done(function(result){
     let film = result.title;
-    $('#filmDiv').append('<div id=' + id_num + '><a onclick="getFilmDetail('+id_num+')">' + film + '</a><br>');
+    $('#filmDiv').append('<div id=' + id_num + 'films' + '><a onclick="getFilmDetail('+id_num+')">' + film + '</a><br>');
   })
 }
 }
@@ -57,10 +64,10 @@ function getFilmDetail(f){
     let opening_crawl = result.opening_crawl;
     let release_date = result.release_date;
     // let characters = result.characters;
-    $('#'+id_num).empty();
-    $('#'+id_num).append(title + '<br>');
-    $('#'+id_num).append(opening_crawl + '<br>');
-    $('#'+id_num).append(release_date + '<br>');
+    $('#'+id_num+"films").empty();
+    $('#'+id_num+"films").append(title + '<br>');
+    $('#'+id_num+"films").append(opening_crawl + '<br>');
+    $('#'+id_num+"films").append(release_date + '<br>');
     // $('#'+id_num).append(characters + '<br>');
     //slice characters later to get top 3
   })
@@ -74,7 +81,7 @@ function getVehicles(v){
   }
   $.ajax(settings).done(function(result){
     let vehicle = result.name;
-    $('#vehicleDiv').append('<div id=' + id_num + '><a onclick="getVehicleDetail('+id_num+')">' + vehicle + '</a><br>');
+    $('#vehicleDiv').append('<div id=' + id_num + 'vehicles' + '><a onclick="getVehicleDetail('+id_num+')">' + vehicle + '</a><br>');
   })
 }
 }
@@ -90,11 +97,11 @@ function getVehicleDetail(v){
     let speed = result.max_atmosphering_speed;
     let vehicle_class = result.vehicle_class;
     let length = result.length;
-    $('#'+id_num).append(name + '<br>');
-    $('#'+id_num).append(model + '<br>');
-    $('#'+id_num).append(speed+ '<br>');
-    $('#'+id_num).append(vehicle_class + '<br>');
-    $('#'+id_num).append(length + '<br>');
+    $('#'+id_num+'vehicles').append(name + '<br>');
+    $('#'+id_num+'vehicles').append(model + '<br>');
+    $('#'+id_num+'vehicles').append(speed+ '<br>');
+    $('#'+id_num+'vehicles').append(vehicle_class + '<br>');
+    $('#'+id_num+'vehicles').append(length + '<br>');
   })
 }
 
